7e67d0872392346625dbb7d6935eae325c18ddaf
JayeshJoshi2005 <jayeshjoshi2005@gmail.com>
2024-07-13 14:13:28 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -5,20 +5,22 @@ import cookieParser from "cookie-parser";
 const app = express();
 //app banne ke baad configure hote hai
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
-
-app.use(express.json({limit:"16kb"}))
-
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-
-
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import 
+import userRouter from './routes/user.routes.js'
 
+//routes declarearion
+app.use("/api/v1/users",userRouter)
 
-export { app };
+export {app};
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,15 @@
-import { request } from "express";
 
-const asyncHandler = (requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(request(req,res,next)).catch((err)=>next(err))
-    }
-}
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 export { asyncHandler };
+
+
+
 
 
 
